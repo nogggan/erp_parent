89ef83c05a8ab6404c687d1fe9e641973b85020e
gan <535211279@qq.com>
2018-08-21 10:14:12 +0800

    整合MyBatis Plus

--- erp_business/src/main/java/com/entor/erp/service/impl/UserServiceImpl.java
+++ erp_business/src/main/java/com/entor/erp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.entor.erp.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.entor.erp.dao.UserMapper;
 import com.entor.erp.entity.User;
@@ -14,8 +15,9 @@ public class UserServiceImpl implements IUserService {
 	private UserMapper userMapper;
 
 	@Override
+	@Transactional
 	public User getById(Integer id) {
-		return userMapper.getById(id);
+		return userMapper.selectById(id);
 	}
 
 }
--- erp_dao/src/main/java/com/entor/erp/dao/UserMapper.java
+++ erp_dao/src/main/java/com/entor/erp/dao/UserMapper.java
@@ -2,10 +2,11 @@ package com.entor.erp.dao;
 
 import org.springframework.stereotype.Repository;
 
+import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.entor.erp.entity.User;
 
 @Repository
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<User>{
 
 	User getById(Integer id);
 	
