b050ea4cfd93ed3d474395a0a1f5be0cd07a3164
gan <535211279@qq.com>
2018-08-22 15:57:31 +0800

    增加模块

---
+++ erp_common/src/main/java/com/entor/erp/result/Result.java
@@ -0,0 +1,33 @@
+package com.entor.erp.result;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain=true)
+public class Result<T> {
+
+	private Integer code;
+	
+	private String msg;
+	
+	private T data;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd hh:mm:ss")
+	private Date timestamp;
+	
+	public static final <E> Result<E> success(E data) {
+		return new Result<E>().setCode(200).setData(data).setMsg("处理成功").setTimestamp(new Date());
+	}
+	
+	public static final <E> Result<E> error(Integer code,String msg){
+		return new Result<E>().setCode(code).setMsg(msg).setTimestamp(new Date());
+	}
+ 	
+}
