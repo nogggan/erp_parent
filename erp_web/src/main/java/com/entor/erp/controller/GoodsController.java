a791643756c42892b0f3e84ddd5fe905186f07f9
gan_gol <gan_gol@qq.com>
2018-08-25 00:33:23 +0800

    完善采购页面

--- erp_web/src/main/java/com/entor/erp/controller/GoodsController.java
+++ erp_web/src/main/java/com/entor/erp/controller/GoodsController.java
@@ -1,8 +1,11 @@
 package com.entor.erp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,11 @@ public class GoodsController {
 	@GetMapping("/{id}")
 	public Goods get(@PathVariable("id")Long id) {
 		return goodsService.selectById(id);
+	}
+	
+	@PostMapping("/all")
+	public List<Goods> getAll(){
+		return goodsService.selectList(null);
 	}
 	
 }
